11ddbee9696e71d4797c112002e9dc9d86c09674
sonia526 <zamirastars3@gmail.com>
2017-09-29 16:02:54 -0500

    mejorando 1

---
+++ js/code.js
@@ -0,0 +1,8 @@
+$("h2").addClass("animated bounceInLeft");
+
+$("img").addClass("animated pulse");
+$(document).ready(function(){
+    $(".cajapadre").click(function(){
+        $(this).hide();
+    });
+});
